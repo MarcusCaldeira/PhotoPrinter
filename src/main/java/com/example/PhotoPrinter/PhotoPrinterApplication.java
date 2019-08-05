b7179faf80e21183d70de61500a240d682cfba58
Marcus Caldeira <marcus@marcuss-mbp.redbrickhealth.local>
2019-08-05 14:51:54 -0500

    First commit

---
+++ src/main/java/com/example/PhotoPrinter/PhotoPrinterApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhotoPrinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoPrinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PhotoPrinter/PhotoPrinterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.PhotoPrinter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoPrinterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
