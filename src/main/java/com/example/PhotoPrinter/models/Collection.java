18b4f4f0940277c01890af67265c08db25cefb9b
MarcusCaldeira <mac.caldr@gmail.com>
2019-08-09 13:36:20 -0500

    there are two partial models, let get this started!

--- src/main/java/com/example/PhotoPrinter/models/Collection.java
+++ src/main/java/com/example/PhotoPrinter/models/Collection.java
@@ -1,4 +1,22 @@
 package com.example.PhotoPrinter.models;
 
+import java.util.Date;
+
 public class Collection {
+
+    private Integer id;
+    private String title;
+    private String description;
+    private int likes;
+    private Date postedDate;
+    private Integer userImageId;
+
+    public Collection(Integer id, String title, String description, int likes, Date postedDate, Integer userImageId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.likes = likes;
+        this.postedDate = postedDate;
+        this.userImageId = userImageId;
+    }
 }
--- src/main/java/com/example/PhotoPrinter/models/Photo.java
+++ src/main/java/com/example/PhotoPrinter/models/Photo.java
@@ -1,4 +1,24 @@
 package com.example.PhotoPrinter.models;
 
+import java.util.Date;
+
 public class Photo {
+
+    private Integer id;
+    private String title;
+    private String description;
+    private String location;
+    private int likes;
+    private Date postedDate;
+    private Integer userImageId;
+
+    public Photo(Integer id, String title, String description, String location, int likes, Date postedDate, Integer userImageId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.likes = likes;
+        this.postedDate = postedDate;
+        this.userImageId = userImageId;
+    }
 }
