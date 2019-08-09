5d02c82823a07568b6ff3cd556e2b7eb1249e8bb
MarcusCaldeira <mac.caldr@gmail.com>
2019-08-09 13:46:25 -0500

    wow I havent put in any arguments into the hash set, and boy is this thing broken. Its red and angry, I dont know how to calm this down

--- src/main/java/com/example/PhotoPrinter/models/Comment.java
+++ src/main/java/com/example/PhotoPrinter/models/Comment.java
@@ -1,4 +1,53 @@
 package com.example.PhotoPrinter.models;
 
+import java.util.Date;
+
 public class Comment {
+
+    private Integer id;
+    private String userName;
+    private String content;
+    private Date postedDate;
+
+    public Comment() {
+    }
+
+    public Comment(Integer id, String userName, String content, Date postedDate) {
+        this.id = id;
+        this.userName = userName;
+        this.content = content;
+        this.postedDate = postedDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(Date postedDate) {
+        this.postedDate = postedDate;
+    }
 }
